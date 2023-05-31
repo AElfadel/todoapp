590b40260ede653ed3fdd16b1dba25665a1fdda4
AElfadel <125569860+AElfadel@users.noreply.github.com>
2023-05-31 19:56:17 +0300

    Completed project

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,68 @@
 "use client";
 
 import Tasks from "@/components/TasksList";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  QuerySnapshot,
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "@/firebase";
 
 export default function Home() {
-  const [tasks, setTasks] = useState([
-    "Code for 3 hours",
-    "Record Dafouri Podcast",
-    "Play FIFA",
-    "Sleep 8 hours",
-    "And if I add another one?",
-  ]);
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  //Create Todo
+
+  const createTask = async (event) => {
+    event.preventDefault(event);
+    if (input === "") {
+      alert("Please enter a task!");
+      return;
+      //Have to add a return to exit the code and not add an empty todo to the database
+    }
+    await addDoc(collection(db, "tasks"), {
+      text: input,
+      compelted: false,
+    });
+    setInput("");
+  };
+
+  //Toggle task dont or not
+  const toggleComplete = async (task) => {
+    await updateDoc(doc(db, "tasks", task.id), {
+      completed: !task.completed,
+    });
+  };
+
+  useEffect(() => {
+    const q = query(collection(db, "tasks"));
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      const todosArray: string[] = [];
+      QuerySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTasks(todosArray);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  //Read Todo from firebase
+
+  //The dependency array is used to stop the function from firing over and over again and causing a memeory leak
+
+  //Update Task in firebase
+
+  //Delete Task
+  const removeTask = async (id) => {
+    await deleteDoc(doc(db, "tasks", id));
+  };
 
   return (
     <main className=" h-screen dark:text-white">
@@ -23,9 +75,11 @@ export default function Home() {
         {/* Add Task Form */}
 
         <div className="bg-gray-200 p-12 w-[600px] m-auto rounded-3xl shadow-lg">
-          <form className="w-[500px] m-auto">
+          <form className="w-[500px] m-auto" onSubmit={createTask}>
             <div className="relative">
               <input
+                value={input}
+                onChange={(event) => setInput(event.target.value)}
                 type="search"
                 id="search"
                 className="w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -43,7 +97,7 @@ export default function Home() {
           {/* Table */}
           <div className="pt-4">
             {/* Head */}
-            <div className="bg-gray-300 w-[500px] m-auto p-2 rounded-t-lg flex justify-between font-bold">
+            <div className="bg-gray-300 w-[504px] m-auto p-2 rounded-t-lg flex justify-between font-bold">
               <p className="px-2">Task</p>
               <p className="px-2">Status</p>
             </div>
@@ -51,9 +105,20 @@ export default function Home() {
             {/* Tasks */}
             <ul className="">
               {tasks.map((task, index) => (
-                <Tasks key={index} task={task} />
+                <Tasks
+                  key={index}
+                  task={task}
+                  toggleComplete={toggleComplete}
+                  removeTask={removeTask}
+                />
               ))}
-              <p className="mt-4">You have 5 tasks to complete</p>
+              <p className="mt-4">
+                {tasks.length === 1
+                  ? `You have ${tasks.length} task to complete`
+                  : tasks.length > 1
+                  ? `You have ${tasks.length} tasks to complete`
+                  : null}
+              </p>
             </ul>
           </div>
         </div>
--- components/TasksList.tsx
+++ components/TasksList.tsx
@@ -1,20 +1,38 @@
+type toggleComplete = {
+  toggleComplete: boolean;
+};
+
 import React from "react";
 import { BsTrash3 } from "react-icons/bs";
 
-function TasksList({ task }: string) {
-  return (
-    <div className="flex flex-row mx-auto p-1 py-2 bg-white w-[500px] justify-between cursor-pointer hover:bg-gray-100">
-      <div className="flex text-start ml-2 ">
-        <input type="checkbox" className="mx-2" />
-        <p className="pl-2">{task}</p>
-      </div>
+const style = {
+  completed: `bg-gray-400 text-black hover:bg-black hover:text-white`,
+};
 
-      <div className="text-right pr-4">
-        <button className="hover:text-red-600">
-          <BsTrash3 />
-        </button>
-      </div>
-    </div>
+function TasksList({ task, toggleComplete, removeTask }) {
+  return (
+    <li className={task.completed ? style.completed : `bg-transparent `}>
+      <li
+        id="carrier"
+        onClick={() => toggleComplete}
+        className="flex flex-row mx-auto p-1 py-2 w-[500px] justify-between cursor-pointer "
+      >
+        <div className="flex text-start ml-2 pl-2">
+          <input
+            onChange={() => toggleComplete(task)}
+            type="checkbox"
+            checked={task.completed ? "checked" : ""}
+          />
+          <p className="pl-2">{task.name || task.text}</p>
+        </div>
+
+        <div className="text-right pr-4">
+          <button className="hover:text-red-600">
+            <BsTrash3 onClick={() => removeTask(task.id)} />
+          </button>
+        </div>
+      </li>
+    </li>
   );
 }
 
